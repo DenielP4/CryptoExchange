c062b2cd12a967f970a6d8828434607e83b701ab
Daniil Popov <popov@example.com>
2023-07-21 14:19:58 +0300

    Create Module Dagger-Hilt

---
+++ app/src/main/java/com/example/cryptoexchange/di/CoinModule.kt
@@ -0,0 +1,35 @@
+package com.example.cryptoexchange.di
+
+import android.app.Application
+import com.example.cryptoexchange.common.Constants
+import com.example.cryptoexchange.data.remote.CoinPaprikaApi
+import com.example.cryptoexchange.data.repository.CoinRepositoryImpl
+import com.example.cryptoexchange.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoinModule {
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinPaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+}
